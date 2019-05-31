b3bec5cb43d117d76e95bd1c6175e27fb24ec070
simonequarticelli <quarti87@gmail.com>
2019-05-31 09:32:20 +0200

    add background + add case null

--- main.js
+++ main.js
@@ -48,192 +48,212 @@ $(document).ready(function(){
   //intercetto il tasto invio
   $('#search').keyup(function(event){
 
-  //salvo la ricerca utente in una variabile
-  var movie_utente = $('#search').val();
-  //console.log(movie_utente);
+    //salvo la ricerca utente in una variabile
+    var movie_utente = $('#search').val();
+    //console.log(movie_utente);
 
-  //intercetto il tasto invio
-  if (event.which == 13) {
+    //intercetto il tasto invio
+    if (event.which == 13) {
 
-    //pulisco la ricerca ad ogni invio
-    $('#search').val(' ');
+      //pulisco la ricerca ad ogni invio
+      $('#search').val(' ');
 
-    //svuoto il contenitore per la nuova ricerca
-    $('.card__container').empty();
+      //svuoto il contenitore per la nuova ricerca
+      $('.card__container').empty();
 
-    /////////////////////////////////////////////////////////////////////////////////////////////////////
-    //chiamata ajax
-    $.ajax({
-      url: url_base_movie,
-      method: 'GET',
-      data: {
-        'api_key': 'd12ae54df472b7dfaec7f47b6ee5fdd3',
-        'query': movie_utente,
-        'language': 'it-IT'
-      },
-      success: function(risposta){
-        //console.log(risposta.results);
+      /////////////////////////////////////////////////////////////////////////////////////////////////////
+      //chiamata ajax
+      $.ajax({
+        url: url_base_movie,
+        method: 'GET',
+        data: {
+          'api_key': 'd12ae54df472b7dfaec7f47b6ee5fdd3',
+          'query': movie_utente,
+          'language': 'it-IT'
+        },
+        success: function(risposta){
+          //console.log(risposta.results);
 
-        //metto dentro a una variabile il contenuto della risposta
-        var contenuto = risposta.results;
-        console.log(contenuto);
+          //metto dentro a una variabile il contenuto della risposta
+          var contenuto = risposta.results;
+          //console.log(contenuto);
 
-        //controllo se il film inserito è presente
-        if (contenuto == '') {
-          Swal.fire({
-            title: 'Movie not find',
-            // text: 'choose another one',
-            type: 'error',
-            confirmButtonText: 'ok'
-          })
-        }
-
-        //eseguo ciclo per scorrere le proprieta
-        for (var i = 0; i < contenuto.length; i++) {
-
-          //locandina
-          var img = contenuto[i].poster_path;
-
-          /////////////////////////////////////////////////////////////////////////////////////////////////////
-          //id film per cast
-          var id = contenuto[i].id;
-          //console.log(id);
-
-          /////////////////////////////////////////////////////////////////////////////////////////////////////
-
-          //aggiungo le serie tv alla ricerca
-          if (contenuto[i].media_type == "tv") {
-            var tipo = contenuto[i].original_name;
-            var nome = contenuto[i].name;
-            var formato = 'TV series';
-          }else{
-            var tipo = contenuto[i].original_title;
-            var nome = contenuto[i].title;
-            var formato = 'Movie';
+          //controllo se il film inserito è presente
+          if (contenuto == '') {
+            Swal.fire({
+              title: 'Movie not find',
+              // text: 'choose another one',
+              type: 'error',
+              confirmButtonText: 'ok'
+            })
           }
 
-          //metto la lingua all'interno di una variabile
-          var lingua = contenuto[i].original_language;
-          //console.log(lingua);
+          //eseguo ciclo per scorrere le proprieta
+          for (var i = 0; i < contenuto.length; i++) {
 
-          /////////////////////////////////////////////////////////////////////////////////////////////////////
+            //locandina
+            var img = contenuto[i].poster_path;
 
-          //fix name flag
-          if (lingua == 'en') {
-            lingua = 'us';
+            var loc = "https://image.tmdb.org/t/p/w342"+img;
+            //console.log(loc);
+
+            if (img == null) {
+              loc = "https://www.macys.com/navapp/web20/assets/img/pdp/productNotAvailable_PDP.png?op_sharpen=1"
+            }else{
+              loc = loc;
+            }
+
+            /////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            //id film per cast
+            var id = contenuto[i].id;
+            //console.log(id);
+
+            /////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            //aggiungo le serie tv alla ricerca
+            if (contenuto[i].media_type == "tv") {
+              var tipo = contenuto[i].original_name;
+              var nome = contenuto[i].name;
+              var formato = 'TV series';
+            }else{
+              var tipo = contenuto[i].original_title;
+              var nome = contenuto[i].title;
+              var formato = 'Movie';
+            }
+
+            //metto la lingua all'interno di una variabile
+            var lingua = contenuto[i].original_language;
             //console.log(lingua);
-          } else if (lingua == 'ja') {
-            lingua = 'jp';
-          }
 
-          var id = i;
-          //console.log(id);
+            /////////////////////////////////////////////////////////////////////////////////////////////////////
 
-          //chiamata per cast
-          $.ajax({
-            url: "https://api.themoviedb.org/3/movie/"+id+"/credits",
-            method: 'GET',
-            data: {
-              movie_id: id,
-              api_key: "d12ae54df472b7dfaec7f47b6ee5fdd3",
-            },
-            success: function(risposta){
-              //console.log(risposta.cast);
-              var cast = risposta.cast;
+            //fix name flag
+            if (lingua == 'en') {
+              lingua = 'us';
+              //console.log(lingua);
+            } else if (lingua == 'ja') {
+              lingua = 'jp';
+            }
+
+            //console.log(lingua);
+
+            if (flg == null) {
+              flag = lingua;
+              console.log(flag);
+            }
+
+            var flg = "https://www.countryflags.io/"+lingua+"/shiny/24.png"
+            console.log(flg);
+
+            var id = i;
+            //console.log(id);
+
+            /////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            //popolo l'oggetto con le informazioni ottenute
+            var movie = {
+              dato1: nome,
+              // dato2: tipo,
+              dato3: flg,
+              dato4: id,
+              dato5: contenuto[i].overview,
+              dato6: formato,
+              dato7: loc,
+            }
+
+            /////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            // typeof <-- specifica il tipo -- si usa con spazio
+
+            //salvo la valutazione del film e la arrotondo per eccesso dividendola per 2
+            var star = parseFloat(contenuto[i].vote_average).toFixed()/2;
+            //console.log(typeof star);
+
+            //limito la valutazione a 5
+            if (star > 5) {
+              star = 5;
+            }
+
+            /////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            //console.log(star);
+            var card__template = $('#card__template').html();
+            var template__function = Handlebars.compile(card__template);
+            var html = template__function(movie);
+
+            //appendo la card del film
+            $('.card__container').append(html);
+
+            //chiamata per cast
+            $.ajax({
+              url: "https://api.themoviedb.org/3/movie/"+id+"/credits",
+              method: 'GET',
+              data: {
+                movie_id: id,
+                api_key: "d12ae54df472b7dfaec7f47b6ee5fdd3",
+              },
+              success: function(risposta){
+                console.log(risposta.cast);
+                var cast = risposta.cast;
+                console.log(cast);
 
 
-              for (var a = 0; a < 5 && a < cast.length; a++) { //<-- condizioni sempre in mezzo
-                var attori = cast[a].name;
-                //console.log(attori);
-                $('.card__movie[data-id="'+i+'"]').find('.actors').append(attori + '<br>');
-                console.log($('.card__movie[data-id="'+i+'"]'));
+
+
+                for (var t = 0; t < 5 && t < cast.length; t++) { //<-- condizioni sempre in mezzo
+                  var attori = cast[t].name;
+                  console.log(attori);
+                  $('.card__movie[data-id="'+i+'"]').find('.actors').append(attori + '<br>');
+                  // console.log($('.card__movie[data-id="'+i+'"]'));
+                }
+
+
+              }, error: function(richiesta, stato, errori){
+                console.log(errori);
               }
 
-
-            }, error: function(richiesta, stato, errori){
-              console.log(errori);
-            }
-          });
-
-          /////////////////////////////////////////////////////////////////////////////////////////////////////
-
-          //popolo l'oggetto con le informazioni ottenute
-          var movie = {
-            dato1: nome,
-            // dato2: tipo,
-            dato3: lingua,
-            dato4: id,
-            dato5: contenuto[i].overview,
-            dato6: formato,
-            dato7: img,
-          }
-
-          /////////////////////////////////////////////////////////////////////////////////////////////////////
-
-          // typeof <-- specifica il tipo -- si usa con spazio
-
-          //salvo la valutazione del film e la arrotondo per eccesso dividendola per 2
-          var star = parseFloat(contenuto[i].vote_average).toFixed()/2;
-          //console.log(typeof star);
-
-          //limito la valutazione a 5
-          if (star > 5) {
-            star = 5;
-          }
-
-          /////////////////////////////////////////////////////////////////////////////////////////////////////
-
-          //console.log(star);
-          var card__template = $('#card__template').html();
-          var template__function = Handlebars.compile(card__template);
-          var html = template__function(movie);
-
-          //appendo la card del film
-          $('.card__container').append(html);
-
-
-
-
-
-          //appendo le stelle
-          for (var s = 0; s < 5; s++) {
-            if (s < star) {
-              $('.card__movie[data-id="'+i+'"]').find('.stars').append('<i class="fas fa-star"></i>');
-            }else{
-              $('.card__movie[data-id="'+i+'"]').find('.stars').append('<i class="far fa-star"></i>');
-            }
-          }
-
-          /////////////////////////////////////////////////////////////////////////////////////////////////////
-
-          $('.info').hide();
-
-
-          $('.card__movie').mouseenter(function(){
-            $(this).find('.poster_path').fadeOut(1000, function(){
-              $(this).parent('.card__movie').addClass('scrool');
-              $(this).siblings('.info').fadeIn();
             });
 
+            //appendo le stelle
+            for (var s = 0; s < 5; s++) {
+              if (s < star) {
+                $('.card__movie[data-id="'+i+'"]').find('.stars').append('<i class="fas fa-star"></i>');
+              }else{
+                $('.card__movie[data-id="'+i+'"]').find('.stars').append('<i class="far fa-star"></i>');
+              }
+            }
 
-          }).mouseleave(function(){
-            $(this).find('.info').fadeOut(1000, function(){
-              $(this).parent('.card__movie').removeClass('scrool');
-              $(this).siblings('.poster_path').fadeIn();
+            /////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            $('.info').hide();
+
+
+            $('.card__movie').mouseenter(function(){
+              $(this).find('.poster_path').fadeOut(200, function(){
+                $(this).parent('.card__movie').addClass('scrool');
+                $(this).siblings('.info').fadeIn();
+              });
+
+
+            }).mouseleave(function(){
+              $(this).find('.info').fadeOut(200, function(){
+                $(this).parent('.card__movie').removeClass('scrool');
+                $(this).siblings('.poster_path').fadeIn();
+              })
             })
-          })
 
+          }
+
+        }, error: function(richiesta, stato, errori){
+          console.log(errori);
         }
 
-      }, error: function(richiesta, stato, errori){
-        console.log(errori);
-      }
+      });
 
-    });
+    }
 
-  }
-
-})
+  })
 
 
 });
